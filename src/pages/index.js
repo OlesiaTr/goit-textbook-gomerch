c8fadb51b55e988c307d1e82f1c583900404dd91
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-01-16 21:25:45 +0200

    refactor: delete index.js from pages folder

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { About } from './About';
-export { Home } from './Home';
-export { Login } from './Login';
-export { NotFound } from './NotFound';
-export { Products } from './Products';
-export { ProductDetails } from './ProductDetails';
